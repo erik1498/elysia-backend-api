e8fef173bec319698bf8664e0db2c49d8b2bc8d8
erik1498 <djefrisonferdinan@gmail.com>
2026-02-17 18:01:49 +0800

    feat (common): fix sumber dana model name

--- src/apps/sumber-dana/sumber-dana.model.ts
+++ src/apps/sumber-dana/sumber-dana.model.ts
@@ -1,6 +1,6 @@
 import { varchar } from "drizzle-orm/mysql-core";
 import { createGenericModel } from "../../common/utils/route-generator.util";
 
-export const sumberDanaTable = createGenericModel("sumber_dana", {
+export const sumberDanaTable = createGenericModel("sumber_dana_tab", {
     nama: varchar("nama", { length: 255 }).notNull(),
 });
